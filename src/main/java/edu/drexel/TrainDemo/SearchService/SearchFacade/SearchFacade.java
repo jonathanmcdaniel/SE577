1c319cea3633c09637d7d7ec5c235232544db836
Jordan Woolley <60066428+Jordan-M-Woolley@users.noreply.github.com>
2020-03-13 20:27:41 -0400

    Create SearchFacade.java
    SearchFacade Interface

---
+++ src/main/java/edu/drexel/TrainDemo/SearchService/SearchFacade/SearchFacade.java
@@ -0,0 +1,15 @@
+// package
+// imports
+
+public interface SearchFacade
+{
+
+    public void indexAllData(Ticket ticket) throws Exception;
+
+    public SearchList search(Ticket ticket, Language language, SearchProductRequest searchRequest);
+
+    public SearchList convertToSearchList(Ticket ticket, Language language, SearchProductRequest searchRequest , int start, int count) throws Exception;
+
+    ValueList autocompleteRequest(String query, Ticket ticket, Language language);
+
+} // End of SearchFacade.
