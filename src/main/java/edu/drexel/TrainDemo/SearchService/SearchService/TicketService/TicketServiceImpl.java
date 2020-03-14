b8312a037bfe46e77f23d1da6ddf596c80b5ec0f
Jordan Woolley <60066428+Jordan-M-Woolley@users.noreply.github.com>
2020-03-13 20:30:52 -0400

    Create TicketServiceImpl.java

---
+++ src/main/java/edu/drexel/TrainDemo/SearchService/SearchService/TicketService/TicketServiceImpl.java
@@ -0,0 +1,61 @@
+// package
+// imports
+
+public class TicketServiceImpl implements TicketService
+{
+
+    private ArrayList<Ticket> tickets;
+
+    public String getPassenger()
+    {
+        // return passenger;
+    }
+
+    public String getDepartureStation();
+    {
+        // return departureStation;
+    }
+
+    public String getArrivalStation();
+    {
+        // return arrivalStation
+    }
+
+    public DateTime getDepartureTime();
+    {
+        // return departureTime;
+    }
+
+    public DateTime getArrivalTime();
+    {
+        // return arrivalTime
+    }
+
+    public String getTrainName();
+    {
+        // return trainName;
+    }
+
+    public String getClass();
+    {
+        // return class;
+    }
+
+    public Double getPrice();
+    {
+        // return price;
+    }
+
+    public Int getDurationTime();
+    {
+        // return durationTime;
+    }
+
+    public Ticket getTicket();
+    {
+        // return ticket
+    }
+
+
+
+} // End of TicketServiceImpl.
