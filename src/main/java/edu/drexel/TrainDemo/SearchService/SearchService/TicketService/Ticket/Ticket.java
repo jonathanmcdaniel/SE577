bec4ebc1ac4b5d2a50d35f60d8f0390690b8630c
Jordan Woolley <60066428+Jordan-M-Woolley@users.noreply.github.com>
2020-03-13 20:31:14 -0400

    Create Ticket.java

---
+++ src/main/java/edu/drexel/TrainDemo/SearchService/SearchService/TicketService/Ticket/Ticket.java
@@ -0,0 +1,22 @@
+// package
+// imports
+
+public class Ticket
+{
+    private String passenger;
+
+    private TrainStation departureStation;
+
+    private TrainStation arrivalStation;
+
+    private DateTime departureTime;
+
+    private DateTime arrivalTime;
+
+    private String trainName;
+
+    private String class;
+
+    private Double price;
+
+} // End of Ticket
