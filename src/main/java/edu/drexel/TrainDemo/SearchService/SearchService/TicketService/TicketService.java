0312b914f01c7fe6720038007c91e62129340c3b
Jordan Woolley <60066428+Jordan-M-Woolley@users.noreply.github.com>
2020-03-13 20:30:35 -0400

    Create TicketService.java

---
+++ src/main/java/edu/drexel/TrainDemo/SearchService/SearchService/TicketService/TicketService.java
@@ -0,0 +1,28 @@
+// package
+// imports
+
+public interface TicketService
+{
+
+    public String getPassenger();
+
+    public String getDepartureStation();
+
+    public String getArrivalStation();
+
+    public DateTime getDepartureTime();
+
+    public DateTime getArrivalTime();
+
+    public String getTrainName();
+
+    public String getClass();
+
+    public Double getPrice();
+
+    public Int getDurationTime();
+
+    public Ticket getTicket();
+
+
+} // End of TicketService.
