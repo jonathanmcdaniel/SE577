f9ca066685a248841d87b64be53b3c9089bcf02b
Lambof3 <60120179+Lambof3@users.noreply.github.com>
2020-03-15 10:19:48 -0400

    Create ListCriteria.java

---
+++ src/main/java/edu/drexel/TrainDemo/models/ListCriteria.java
@@ -0,0 +1,24 @@
+package edu.drexel.TrainDemo.models
+
+/* Used to filter lists */
+
+public class listCriteria {
+	private String name;
+	private String type;
+	
+	public String getType() {
+		return type;
+	}
+	
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name=name;
+	}
+}
