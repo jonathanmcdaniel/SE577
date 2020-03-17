7d468f057cc1fb092c254bb4a0c62f7a95ea3288
cmsho <55258597+cmsho@users.noreply.github.com>
2020-03-17 18:57:44 -0400

    Commented out non-functional code

--- src/main/java/edu/drexel/TrainDemo/models/Payment.java
+++ src/main/java/edu/drexel/TrainDemo/models/Payment.java
@@ -1,53 +1,53 @@
-package edu.drexel.TrainDemo.models;
+// package edu.drexel.TrainDemo.models;
 
-import java.math.BigDecimal;
-import java.util.Map;
+// import java.math.BigDecimal;
+// import java.util.Map;
 
-public class Payment {
+// public class Payment {
 	
-	private PaymentType paymentType;
-	private TransactionType transactionType = TransactionType.AUTHORIZECAPTURE;
-	private Currency currency;
-	private BigDecimal amount;
-	private Map<String, String> paymentMetaData = null;
+// 	private PaymentType paymentType;
+// 	private TransactionType transactionType = TransactionType.AUTHORIZECAPTURE;
+// 	private Currency currency;
+// 	private BigDecimal amount;
+// 	private Map<String, String> paymentMetaData = null;
 
-	public PaymentType getPaymentType() {
-		return paymentType;
-	}
+// 	public PaymentType getPaymentType() {
+// 		return paymentType;
+// 	}
 	
-	public void setPaymentType(PaymentType paymentType) {
-		this.paymentType = paymentType;
-	}
+// 	public void setPaymentType(PaymentType paymentType) {
+// 		this.paymentType = paymentType;
+// 	}
 	
-	public TransactionType getTransactionType() {
-		return transactionType;
-	}
+// 	public TransactionType getTransactionType() {
+// 		return transactionType;
+// 	}
 	
-	public void setTransactionType(TransactionType transactionType) {
-		this.transactionType = transactionType;
-	}
+// 	public void setTransactionType(TransactionType transactionType) {
+// 		this.transactionType = transactionType;
+// 	}
 	
-	public Currency getCurrency() {
-		return currency;
-	}
+// 	public Currency getCurrency() {
+// 		return currency;
+// 	}
 	
-	public void setCurrency(Currency currency) {
-		this.currency = currency;
-	}
+// 	public void setCurrency(Currency currency) {
+// 		this.currency = currency;
+// 	}
 	
-	public BigDecimal getAmount() {
-		return amount;
-	}
+// 	public BigDecimal getAmount() {
+// 		return amount;
+// 	}
 	
-	public void setAmount(BigDecimal amount) {
-		this.amount = amount;
-	}
+// 	public void setAmount(BigDecimal amount) {
+// 		this.amount = amount;
+// 	}
 	
-	public Map<String,String> getPaymentMetaData(){
-		return paymentMetaData;
-	}
+// 	public Map<String,String> getPaymentMetaData(){
+// 		return paymentMetaData;
+// 	}
 	
-	public void setPaymentMetaData(Map<String, String> paymentMetaData) {
-		this.paymentMetaData = paymentMetaData;
-	}
-}
+// 	public void setPaymentMetaData(Map<String, String> paymentMetaData) {
+// 		this.paymentMetaData = paymentMetaData;
+// 	}
+// }
--- src/main/java/edu/drexel/TrainDemo/models/PaymentModule.java
+++ src/main/java/edu/drexel/TrainDemo/models/PaymentModule.java
@@ -1,30 +1,30 @@
-package edu.drexel.TrainDemo.models;
+// package edu.drexel.TrainDemo.models;
 
-import java.math.BigDecimal;
-import java.util.List;
+// import java.math.BigDecimal;
+// import java.util.List;
 
-public class PaymentModule {
+// public class PaymentModule {
 	
-	public void validateModule(IntegrationConfiguration integrationConfiguration, TrainTicket ticket) throws IntegrationException;
+// 	public void validateModule(IntegrationConfiguration integrationConfiguration, TrainTicket ticket) throws IntegrationException;
 	
-	public Transaction initTransaction(
-			TrainTicket ticket, Passenger passenger, BigDecimal amount, Payment payment, IntegrationConfiguration configuration, IntegrationModule module)
-					throws IntegrationException;
+// 	public Transaction initTransaction(
+// 			TrainTicket ticket, Passenger passenger, BigDecimal amount, Payment payment, IntegrationConfiguration configuration, IntegrationModule module)
+// 					throws IntegrationException;
 	
-	public Transaction authorize(
-			TrainTicket ticket, Passenger passenger, List<ShoppingCartItem> item, BigDecimal amount, Payment payment, IntegrationConfiguration configuration, IntegrationModule module)
-					throws IntegrationException;
+// 	public Transaction authorize(
+// 			TrainTicket ticket, Passenger passenger, List<ShoppingCartItem> item, BigDecimal amount, Payment payment, IntegrationConfiguration configuration, IntegrationModule module)
+// 					throws IntegrationException;
 	
-	public Transaction capture(
-			TrainTicket ticket, Passenger passenger, Order order, Transaction capturableTransaction, IntegrationConfiguration configuration, IntegrationModule module)
-					throws IntegrationException;
+// 	public Transaction capture(
+// 			TrainTicket ticket, Passenger passenger, Order order, Transaction capturableTransaction, IntegrationConfiguration configuration, IntegrationModule module)
+// 					throws IntegrationException;
 	
-	public Transaction authorizeAndCapture(
-			TrainTicket ticket, Passenger passenger, List<ShoppingCartItem> item, BigDecimal amount, Payment payment, IntegrationConfiguration configuration, IntegrationModule module)
-					throws IntegrationException;
+// 	public Transaction authorizeAndCapture(
+// 			TrainTicket ticket, Passenger passenger, List<ShoppingCartItem> item, BigDecimal amount, Payment payment, IntegrationConfiguration configuration, IntegrationModule module)
+// 					throws IntegrationException;
 	
-	public Transaction refund(
-			boolean partial, TrainTicket ticket, Transaction transaction, Order order, BigDecimal amount,IntegrationConfiguration configuration, IntegrationModule module)
-					throws IntegrationException;
+// 	public Transaction refund(
+// 			boolean partial, TrainTicket ticket, Transaction transaction, Order order, BigDecimal amount,IntegrationConfiguration configuration, IntegrationModule module)
+// 					throws IntegrationException;
 
-}
+// }
